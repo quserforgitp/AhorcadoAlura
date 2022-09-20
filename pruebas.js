2ce2fce8bdd51a06d8d3ee560edb1f043db6b009
Helios <adealumnonegligas@gmail.com>
2022-09-20 15:08:35 -0500

    tranformar texto a uppercase con css

--- pruebas.js
+++ pruebas.js
@@ -1,5 +1,8 @@
 var areaTexto = document.querySelector("#areaTexto");
+var areaTexto2 = document.querySelector("#areaTexto-2");
 
 areaTexto.addEventListener("keyup", (e) => {
     areaTexto.value = areaTexto.value.toUpperCase()
-})
+}) // transformar texto a uppercase (se alcanza a ver el lowercase)
+
+areaTexto2.style.textTransform = "uppercase"; // transformar texto a uppercase
